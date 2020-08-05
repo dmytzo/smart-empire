b3df0ff990de6f1e4445507a00a91bb1b7ee5ff1
dmytzo <dmitriytris@gmail.com>
2020-08-06 02:27:25 +0300

    Update tg_bot handlers

--- tg_bot/handlers.go
+++ tg_bot/handlers.go
@@ -6,24 +6,39 @@ import (
 	"smart_empire/sensors"
 )
 
+var TemperatureSensorCurrentState string
+var DoorSensorCurrentState string
+
 func SensorsHandler(bot *tgbotapi.BotAPI) {
 	for {
 		select {
 		case ds := <- sensors.DoorSensor.MsgChan:
 			doorSensorHandler(bot, ds)
+		case ts := <- sensors.TemperatureSensor.MsgChan:
+			thermometerSensorHandler(bot, ts)
 		}
 	}
 }
 
 func doorSensorHandler(bot *tgbotapi.BotAPI, dsMsg sensors.DoorSensorMsg) {
-	event := "door is closed"
+	event := "closed"
 	if dsMsg.Contact == false {
-		event = "door is opened"
+		event = "opened"
 	}
-	msg := fmt.Sprintf("*DoorSensor:* %s", event)
+	msg := fmt.Sprintf("*DoorSensor:* \n%s", event)
+	DoorSensorCurrentState = msg
 	for _, user := range GetAuth().getActiveUsers() {
 		msg := tgbotapi.NewMessage(user.ChatId, msg)
 		msg.ParseMode = "Markdown"
 		bot.Send(msg)
 	}
+}
+
+func thermometerSensorHandler(bot *tgbotapi.BotAPI, dsMsg sensors.TemperatureSensorMsg) {
+	msg := fmt.Sprintf(
+		"*TemperatureSensor:* \n" +
+			"%.1f ℃ / %.1f %%",
+			dsMsg.Temperature, dsMsg.Humidity,
+		)
+	TemperatureSensorCurrentState = msg
 }
